e848df48af21f19999b9678a7a9a1bd1830d2caf
Jenny Feng <fengchej@mcmaster.ca>
2017-01-17 14:19:17 -0500

    Add files via upload

---
+++ Rectangles.py
@@ -0,0 +1,57 @@
+from turtle import*
+from math import*
+def nestedSquares():
+    reset()
+    hideturtle()
+    x=-200;y=200
+    penup()
+    setposition(x,y)
+    pendown()
+    right(90)
+    for i in range(200,-1,-20):
+        for a in range(4):
+            forward(i)
+            left(90)
+        x+=5;y-=5
+        penup()
+        setposition(x,y)
+        pendown()
+        
+def artisticSquares():
+    reset()
+    hideturtle()
+    penup()
+    setposition(-200,100)
+    pendown()
+    left(90)
+    length=100
+    n=['purple','grey']
+    for i in range(7):
+        for j in range(8):
+            begin_fill()
+            color(n[i%2])
+            for k in range(4):
+                pencolor(n[(i+1)%2])
+                pensize(length/30)
+                forward(length)
+                right(90)
+            end_fill()   
+            penup()
+            right(45)
+            forward(sqrt(2*length**2))
+            pendown()
+        right(135)
+        penup()
+        forward(length)
+        pendown()
+        left(157.5)
+        length=length/1.847
+        
+
+
+
+
+
+
+        
+    
---
+++ Squares.py
@@ -0,0 +1,122 @@
+from turtle import*
+def adjacentRectangles():
+    reset()
+    hideturtle()
+    right(90)
+    width=512
+    g=98
+    for i in range(9):
+        begin_fill()
+        color('grey'+str(g))
+        for j in range(2):
+            pencolor('red')
+            forward(300)
+            left(90)
+            forward(width)
+            left(90)
+        end_fill()
+        width=width/2
+        g=g-14
+        
+def atisticRectangles():
+    reset()
+    bgcolor("black")
+    hideturtle()
+    penup()
+    setposition(0,-30)
+    pendown()
+    b=0
+    for a in range(2):
+        for i in range(4):
+            n=14
+            for j in range(4):
+                begin_fill()
+                color('grey'+str(n))
+                n+=14
+                pencolor('black')
+                if b!=0 or a!=0:
+                    if b==0:
+                        penup()
+                        left(90)
+                        forward(30)
+                        right(90)
+                        pendown()
+                    for k in range(2):  
+                        forward(30)
+                        left(90)
+                        forward(90)
+                        left(90)
+                elif b == 0:
+                    for k in range(2):
+                        forward(30)
+                        left(90)
+                        forward(120)
+                        left(90)
+                        b=0
+                else:
+                    b
+                left(90)
+                end_fill()
+                penup()
+                if a==1:
+                    b=1
+                if b==0:  
+                    forward(30)
+                    
+                else:
+                    forward(30)
+                right(90)
+                if a==0:
+                    backward(30)
+                else:
+                    forward(30)
+                pendown()
+            penup()
+            if a ==0:
+                forward(150)
+            else:
+                backward(90)
+            right(90)
+            if a==0:
+                forward(90)
+            else:
+                forward(150)
+            pendown()
+            if a==0:
+                b=0
+            else:
+                b=1
+        penup()
+        if a==0:
+            forward(30)
+        else:
+            forward(150)
+        pendown()
+
+
+        
+
+
+
+
+
+
+        
+
+        
+
+
+
+
+
+
+                
+                
+
+                
+
+
+
+
+
+    
